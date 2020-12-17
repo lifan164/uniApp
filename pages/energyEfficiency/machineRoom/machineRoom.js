7d4b6f72c7b34da77df038716e2dac4bb09356f2
shixin <shixin@zhiyihealth.com.cn>
2020-12-17 22:18:20 +0800

    1

---
+++ pages/energyEfficiency/machineRoom/machineRoom.js
@@ -0,0 +1,61 @@
+import Calendar from '@/components/ren-calendar/ren-calendar.vue';
+export default {
+	components: {
+		Calendar
+	},
+	data() {
+		return {
+			markShowList: [{
+				color: '#FF0005',
+				textVal: '急需改善'
+			}, {
+				color: '#F5D812',
+				textVal: '一般'
+			}, {
+				color: '#279DE0',
+				textVal: '良好'
+			}, {
+				color: '#1FE812',
+				textVal: '优势'
+			}],
+			markDays:[{
+				time:'2020-12-18',
+				showClass: 'redClass'
+			},{
+				time:'2020-12-19',
+				showClass: 'yellowClass'
+			},{
+				time:'2020-12-20',
+				showClass: 'blueClass'
+			}],
+			curDate:'',
+		}
+	},
+	onLoad() {
+		
+	},
+	onReadly(){
+		let today = this.$refs.ren.getToday().date;
+		this.curDate = today;
+		this.markDays.push(today);
+	},
+	methods: {
+		onDayClick:function (data){
+			this.curDate = data.date;
+		},
+		changeMonth: function (data){
+			console.log(data)
+			this.markDays = [{
+				time:`${data.nowMonth}-20`,
+				showClass: 'yellowClass'
+			},{
+				time:`${data.nowMonth}-22`,
+				showClass: 'redClass'
+			},{
+				time:`${data.nowMonth}-23`,
+				showClass: 'blueClass'
+			}]
+			console.log(this.markDays)
+		}
+	}
+}
--- pages/energyEfficiency/machineRoom/machineRoom.vue
+++ pages/energyEfficiency/machineRoom/machineRoom.vue
@@ -1,22 +1,27 @@
 <template>
 	<view>
-		鑫鑫加油
+		<view class="item_box">帆帆，爱你呦！</view>
+		<view class="item_box">
+			<view class="title">实时能效</view>
+		</view>
+		<view class="item_box">
+			<view class="title">能效日历</view>
+			<view class="mark_box">
+				<view class="mark_item" v-for="(item,index) in markShowList" :key="index">
+					<view class="radius_box" v-bind:style="{'background': item.color}"></view>
+					<view>{{item.textVal}}</view>
+				</view>
+			</view>
+			<view class="Calendar_box">
+				<Calendar ref='ren' :markDays='markDays' :headerBar='true' @onDayClick='onDayClick'
+				@changeMonth='changeMonth'></Calendar>
+			</view>
+		</view>
 	</view>
+
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
-</script>
-
+<script src="./machineRoom.js"></script>
 <style>
-
+	@import url("./machineRoom.css");
 </style>
